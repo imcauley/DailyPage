798eb0a2c0408c16beb7d65abfee19dbc2968b92
Isaac <mcauleyisaac@gmail.com>
2021-02-16 21:46:08 -0700

    added toggle for sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,40 @@
 <template>
 <v-app>
-  <main-nav>
-  </main-nav>
+    <v-navigation-drawer 
+    v-model="drawer"
+    app
+    >
+    <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title">
+            Daily
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            Your daily planner
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+    <v-divider></v-divider>
+
+    <v-tabs vertical>
+      <v-tab to="/today">
+        Today
+      </v-tab>
+      <v-tab to="/history">
+        History
+      </v-tab>
+      <v-tab to="/edit-template">
+        Edit Template
+      </v-tab>
+    </v-tabs>
+
+  </v-navigation-drawer>
 
   <v-app-bar app>
+    <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
   </v-app-bar>
 
-   <v-navigation-drawer
-      v-model="drawer"
-      absolute
-      bottom
-      temporary
-    >
-    </v-navigation-drawer>
 
   <!-- Sizes your content based upon application components -->
   <v-main>
@@ -27,7 +49,7 @@
 </template>
 
 <script>
-import MainNav from './components/MainNav';
+// import MainNav from './components/MainNav';
 // import Writing from './components/Writing.vue';
 
 export default {
@@ -35,11 +57,11 @@ export default {
   name: 'App',
 
   components: {
-    MainNav,
+    // MainNav,
   },
 
   data: () => ({
-    //
+    drawer: false
   }),
 };
 </script>
