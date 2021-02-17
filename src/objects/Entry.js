371f96c23c66a96aec8fded5738fae431c568fd5
Isaac <mcauleyisaac@gmail.com>
2021-02-16 21:41:44 -0700

    made it electron

--- src/objects/Entry.js
+++ src/objects/Entry.js
@@ -1,10 +1,10 @@
-class Entry {
-    constructor(template, date) {
-        this.text = template
-        this.created = date
-    }
+// class Entry {
+//     constructor(template, date) {
+//         this.text = template
+//         this.created = date
+//     }
 
-    addText(text) {
-        this.text = text
-    }
-}
+//     addText(text) {
+//         this.text = text
+//     }
+// }
